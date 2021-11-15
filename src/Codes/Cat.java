a24c0cc9799544e488fae833ff4a0ac79abd96fc
koolunkle <gugi329@daum.net>
2021-11-15 23:11:00 +0900

    Cat 클래스 작성 -> toString/equals 재정의(override)

--- src/Codes/Cat.java
+++ src/Codes/Cat.java
@@ -8,4 +8,25 @@ public class Cat extends Animal {
 		System.out.println("고양이가 생선을 먹습니다.");
 	}
 
+	@Override
+	public String toString() {
+
+		String message = "이름 : " + this.name + " , " + "출생연도 : " + this.birthYear;
+		return message;
+
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		Cat otherCat = (Cat) obj;
+
+		if (this.name == otherCat.name && this.birthYear == otherCat.birthYear) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
 }
