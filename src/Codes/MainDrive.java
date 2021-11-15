a9fad7637350376db8dd55a4c700c87c4456fc75
koolunkle <gugi329@daum.net>
2021-11-15 22:52:26 +0900

    MainDrive 클래스 작성 > 재정의(override) 확인

--- src/Codes/MainDrive.java
+++ src/Codes/MainDrive.java
@@ -4,6 +4,17 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 
+		Dog dog1 = new Dog();
+		dog1.name = "바둑이";
+		dog1.birthYear = 2015;
+
+		Cat cat1 = new Cat();
+		cat1.name = "나옹이";
+		cat1.birthYear = 2018;
+
+		dog1.eat();
+		cat1.eat();
+
 	}
 
 }
